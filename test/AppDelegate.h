7d7f553853e53ee779b4def49b0611b6f1590bdc
Tonito <tonito@tokopedia.com>
2015-04-13 09:21:25 +0700

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Tokopedia on 4/13/15.
+//  Copyright (c) 2015 Tokopedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by Tokopedia on 4/13/15.
+//  Copyright (c) 2015 Tokopedia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
